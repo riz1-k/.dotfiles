e44754a16ae09a364a27075b95e561dff92b0553
riz <rizwan.k.off@gmail.com>
2024-09-26 10:30:38 +0530

    first commit

---
+++ .wezterm.lua
@@ -0,0 +1,25 @@
+local wezterm = require("wezterm")
+
+local config = wezterm.config_builder()
+
+config.font = wezterm.font("JetBrainsMono Nerd Font")
+config.font_size = 11
+
+config.enable_tab_bar = false
+config.window_decorations = "RESIZE"
+
+config.colors = {
+	foreground = "#CBE0F0",
+	background = "#011423",
+	cursor_bg = "#47FF9C",
+	cursor_border = "#47FF9C",
+	cursor_fg = "#011423",
+	selection_bg = "#033259",
+	selection_fg = "#CBE0F0",
+	ansi = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#0FC5ED", "#a277ff", "#24EAF7", "#24EAF7" },
+	brights = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#A277FF", "#a277ff", "#24EAF7", "#24EAF7" },
+}
+
+config.window_background_opacity = 0.8
+
+return config
