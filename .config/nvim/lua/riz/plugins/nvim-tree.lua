22774651e521228b0a3113a2001a8f0270b55f8c
riz <rizwan.k.off@gmail.com>
2024-11-19 08:11:56 +0530

    updated cmp to better usage

---
+++ .config/nvim/lua/riz/plugins/nvim-tree.lua
@@ -0,0 +1,66 @@
+return {
+	"nvim-tree/nvim-tree.lua",
+	dependencies = "nvim-tree/nvim-web-devicons",
+	config = function()
+		local nvimtree = require("nvim-tree")
+
+		-- recommended settings from nvim-tree documentation
+		vim.g.loaded_netrw = 1
+		vim.g.loaded_netrwPlugin = 1
+
+		nvimtree.setup({
+			view = {
+				width = 30,
+				adaptive_size = true,
+				-- 	float = {
+				-- 		enable = true,
+				-- 		quit_on_focus_loss = true,
+				-- 		open_win_config = {
+				-- 			relative = "editor",
+				-- 			border = "rounded",
+				-- 			width = 30,
+				-- 			height = 50,
+				-- 			row = 1,
+				-- 			col = 1,
+				-- 		},
+				-- 	},
+				-- 	relativenumber = true,
+			},
+			-- change folder arrow icons
+			renderer = {
+				indent_markers = {
+					enable = true,
+				},
+				icons = {
+					glyphs = {
+						folder = {
+							arrow_closed = "", -- arrow when folder is closed
+							arrow_open = "", -- arrow when folder is open
+						},
+					},
+				},
+			},
+			-- disable window_picker for
+			-- explorer to work well with
+			-- window splits
+			actions = {
+				open_file = {
+					window_picker = {
+						enable = false,
+					},
+				},
+			},
+			filters = {
+				custom = { ".DS_Store", "node_modules", ".git", ".vscode", ".github", ".idea", ".next" },
+			},
+			git = {
+				ignore = false,
+			},
+		})
+
+		-- set keymaps
+		local keymap = vim.keymap -- for conciseness
+
+		keymap.set("n", "<leader>e", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle file explorer " })
+	end,
+}
---
+++ .config/nvim/lua/riz/plugins/supermaven.lua
@@ -0,0 +1,7 @@
+return {
+	"supermaven-inc/supermaven-nvim",
+	enabled = true,
+	config = function()
+		require("supermaven-nvim").setup({})
+	end,
+}
